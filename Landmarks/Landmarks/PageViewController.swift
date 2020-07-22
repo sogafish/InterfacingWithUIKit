bbe4067cccf19c502726fcad7fc52b7b29ec386a
sogafish <k.sogafish@gmail.com>
2020-07-23 01:45:35 +0900

    412 Create the View Controller's Data Source

--- Landmarks/Landmarks/PageViewController.swift
+++ Landmarks/Landmarks/PageViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 
 struct PageViewController: UIViewControllerRepresentable {
     var controllers: [UIViewController]
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
 
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewContoller = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal
         )
+        pageViewContoller.dataSource = context.coordinator
         
         return pageViewContoller
     }
@@ -25,5 +30,40 @@ struct PageViewController: UIViewControllerRepresentable {
         pageViewController.setViewControllers(
             [controllers[0]], direction: .forward, animated: true
         )
+    }
+    
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        
+        init(_ pageViewController: PageViewController) {
+            self.parent = pageViewController
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+                guard let index = parent.controllers.firstIndex(of: viewController) else {
+                    return nil
+                }
+                
+                if index == 0 {
+                    return parent.controllers.last
+                }
+                
+                return parent.controllers[index - 1]
+            }
+
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let index = parent.controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+
+            if index + 1 == parent.controllers.count {
+                return parent.controllers.first
+            }
+            return parent.controllers[index + 1]
+        }
     }
 }
